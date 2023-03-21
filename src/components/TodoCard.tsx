a61a3b6f07fa9316042237ade96e7d3b09131fcf
Adrian <adrian@MacBook-Pro.local>
2023-03-21 09:40:43 +0200

    Fixed delete todo button and added filters

--- src/components/TodoCard.tsx
+++ src/components/TodoCard.tsx
@@ -6,14 +6,15 @@ import {ButtonGroup, Button} from "@mui/material";
 import {v4 as uuidv4} from "uuid";
 
 function TodoCard() {
-    const [todos, setTodos] = useState<Array<React.ReactElement>>([]);
+    const [todos, setTodos]: any = useState([]);
 
     function handleDelete(id: string) {
-        console.log(todos[0].props.id);
-        // todos.forEach(function(todo){
-        //     console.log("todo",todo.props.id);
-        //     console.log("id",id);
+        // todos.forEach((todo) => {
+        //     // console.log("todo",todo.props.id);
+        //     // console.log("id",id);
+        //
         // })
+        // console.log(todos)
         // let todoToDelete = todos.findIndex(function(todo) {
         //     console.log("todo",todo.props.id);
         //     console.log("id",id);
@@ -22,32 +23,24 @@ function TodoCard() {
         // );
         // let newTodos = todos.splice(todoToDelete,1);
         // setTodos(newTodos);
+        // //
+
+        const newArray = todos.filter((todo: any) => todo.id !== id);
+        setTodos(newArray);
     }
 
-    const handleCheckboxChange = (e: any) => {
-        todos.map((todo) => {
-            console.log(todo.props.control);
+    const handleCheckboxChange = (id: string) => {
+        console.log(id);
+        todos.forEach((todo: any) => {
+            if (todo.id === id) {
+                todo.checked = !todo.checked;
+            }
         })
     };
 
     function keyPress(e: any) {
         if (e.keyCode === 13) {
-            const id = uuidv4();
-            const newTodo = (
-                <span
-                    style={{display: "flex", alignItems: "center"}}
-                    key={id}
-                    id={id}
-                >
-          <FormControlLabel
-              style={{wordWrap: "break-word"}}
-              control={<Checkbox onChange={handleCheckboxChange}/>}
-              label={e.target.value}
-          />
-          <CloseIcon onClick={() => handleDelete(id)}/>
-        </span>
-            );
-            setTodos([...todos, newTodo]);
+            setTodos((todos: any) => [...todos, {"name": e.target.value, "id": uuidv4(), "checked": false}]);
             e.target.value = "";
         }
     }
@@ -80,7 +73,56 @@ function TodoCard() {
                 <TextField style={{marginBottom: '10px'}} id="change-title" label="Change Title" variant="standard"/>
                 <TextField style={{margin: '10px'}} id="add-task" label="Add new task" variant="standard"
                            onKeyDown={keyPress}/>
-                {todos}
+                {showAll && todos.map((todo: any) => {
+
+                    if(todo.checked) {
+                        return (
+                            <span style={{display: "flex", alignItems: "center"}} key={todo.id}>
+                        <FormControlLabel style={{wordWrap: "break-word"}}
+                                          control={<Checkbox defaultChecked = {true} onChange={() => handleCheckboxChange(todo.id)}/>}
+                                          label={todo.name}/>
+                        <CloseIcon onClick={() => handleDelete(todo.id)}/>
+                        </span>
+                        )
+                    }
+
+                    return (
+                        <span style={{display: "flex", alignItems: "center"}} key={todo.id}>
+                        <FormControlLabel style={{wordWrap: "break-word"}}
+                                          control={<Checkbox onChange={() => handleCheckboxChange(todo.id)}/>}
+                                          label={todo.name}/>
+                        <CloseIcon onClick={() => handleDelete(todo.id)}/>
+                        </span>
+                    )
+                })}
+
+                {/* eslint-disable-next-line array-callback-return */}
+                {showCompleted && todos.map((todo: any) => {
+                    if (todo.checked) {
+                    return (
+                        <span style={{display: "flex", alignItems: "center"}} key={todo.id}>
+                        <FormControlLabel style={{wordWrap: "break-word"}}
+                                          control={<Checkbox defaultChecked = {true} onChange={() => handleCheckboxChange(todo.id)}/>}
+                                          label={todo.name}/>
+                        <CloseIcon onClick={() => handleDelete(todo.id)}/>
+                        </span>
+                    )}
+
+                })}
+
+                {/* eslint-disable-next-line array-callback-return */}
+                {showIncomplete && todos.map((todo: any) => {
+                    if(!todo.checked) {
+                        return (
+                            <span style={{display: "flex", alignItems: "center"}} key={todo.id}>
+                        <FormControlLabel style={{wordWrap: "break-word"}}
+                                          control={<Checkbox onChange={() => handleCheckboxChange(todo.id)}/>}
+                                          label={todo.name}/>
+                        <CloseIcon onClick={() => handleDelete(todo.id)}/>
+                        </span>
+                        )
+                    }
+                })}
                 <ButtonGroup
                     color="primary"
                     aria-label="outlined primary button group"
